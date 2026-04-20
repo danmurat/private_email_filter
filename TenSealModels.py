fixed pca data leakage + added dimensionality testing

TenSealModels.py
@@ -14,19 +14,19 @@ class TenSealModels:
     def __init__(self):
         # from tutorial. This holds all the encryption keys + operations we can do
         # we can test CKKS scheme too!
-        self.context = ts.context(ts.SCHEME_TYPE.BFV, poly_modulus_degree=4096, plain_modulus=1032193)
+        #self.context = ts.context(ts.SCHEME_TYPE.BFV, poly_modulus_degree=4096, plain_modulus=1032193)
         self.c = 200.0 # most accurate soft-margin param found so far for SVM
 
     def trainLog(self, X_train, y_train, epochs):
         model = LogisticReg(X_train.shape[1])
         optim = torch.optim.SGD(model.parameters(), lr=1) # gradient descent 
-        criterion = torch.nn.BCELoss() # Binary Cross Entropy Loss
+        l = torch.nn.BCELoss() # Binary Cross Entropy Loss
 
         # typical "minimise loss", with pytorch handling most of the details..
         for e in range(epochs + 1):
             optim.zero_grad()
             out = model(X_train)
-            loss = criterion(out, y_train)
+            loss = l(out, y_train)
             loss.backward()
             optim.step()
             

util.py
@@ -4,6 +4,8 @@ import pandas as pd
 import json
 import tenseal as ts
 from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import StandardScaler
 
 """
 Contains any useful functions to be used anywhere in our code.
@@ -18,23 +20,38 @@ def loadModelPickle(name):
     with open(f"pickle_objects/{name}.pkl", "rb") as file:
         return pickle.load(file)
 
-def convertToTorchTensors(X_train, y_train, X_test, y_test, red_X_train, red_X_test) -> tuple:
+def convertToTorchTensors(X_train, y_train, X_test, y_test, pca_red_X_train, pca_red_X_test, svd_red_X_train, svd_red_X_test) -> tuple:
     t_X_train = torch.tensor(X_train).float()
     t_y_train = torch.tensor(y_train).float().unsqueeze(1) # adds extra dimension [n, 1] so that it corresponds with X when training
     t_X_test = torch.tensor(X_test).float()
     t_y_test = torch.tensor(y_test).float().unsqueeze(1)
-    t_red_X_train = torch.tensor(red_X_train).float()
-    t_red_X_test = torch.tensor(red_X_test).float()
+    t_pca_red_X_train = torch.tensor(pca_red_X_train).float()
+    t_pca_red_X_test = torch.tensor(pca_red_X_test).float()
+    t_svd_red_X_train = torch.tensor(svd_red_X_train).float()
+    t_svd_red_X_test = torch.tensor(svd_red_X_test).float()
 
-    return t_X_train, t_y_train, t_X_test, t_y_test, t_red_X_train, t_red_X_test
+    return t_X_train, t_y_train, t_X_test, t_y_test, t_pca_red_X_train, t_pca_red_X_test, t_svd_red_X_train, t_svd_red_X_test
 
 # pca was saved with n_components=200
-def pca_data(X_train, X_test) -> tuple:
-    pca = loadModelPickle("pca")
-    red_X_train = pca.transform(X_train)
-    red_X_test = pca.transform(X_test)
+def pca_data(X_train, X_test, n_comp) -> tuple:
+    pca = PCA(n_components=n_comp)
+    # fit FOR BOTH! Otherwise data will leak to test set
+    red_X_train = pca.fit_transform(X_train)
+    red_X_test = pca.fit_transform(X_test)
 
     return red_X_train, red_X_test
+
+def svd_data(X_train, X_test, n) -> tuple:
+    scaler = StandardScaler()
+    svd = TruncatedSVD(n_components=n)
+    red_X_train = svd.fit_transform(X_train)
+    red_X_test = svd.fit_transform(X_test)
+
+    red_X_train_scaled = scaler.fit_transform(red_X_train)
+    red_X_test_scaled= scaler.fit_transform(red_X_test)
+
+    return red_X_train_scaled, red_X_test_scaled
+
 
 # incase we ever need to run again/change params
 def train_pca():